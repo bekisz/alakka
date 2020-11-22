dc1d07f1676b434ae28eee5311cbd31852257274
Szabolcs Beki <szabi@apache.org>
2020-11-22 20:38:51 +0100

    Created a very simple coin toss experiement.

--- src/main/scala/org/alakka/Experiment.scala
+++ src/main/scala/org/alakka/Experiment.scala
@@ -5,7 +5,7 @@ import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 
 
 object Experiment {
-  trait Command
+  trait Command extends Trial.Response
   trait Status
   case class StartCommand() extends Command
   case class RunningStatus() extends Status {
@@ -24,11 +24,17 @@ class Experiment(context: ActorContext[Experiment.Command]) extends AbstractBeha
   //import ._
 
   //private var n = 0
+
+
   override def onMessage(msg: Experiment.Command): Behavior[Experiment.Command] = {
     msg match {
       case Experiment.StartCommand() =>
         context.log.debug("Experiment Started")
         println("Experiment Started")
+
+        val trial = context.spawn(Trial(), "trial1")
+        trial ! Trial.StartRequest(10)
+        // this.context.self
         context.log.debug(s"Started ${context.self.path.name} with ${context.self.path}")
         println(s"Started  context.self.path : ${context.self.path.name} with ${context.self.path}")
         println(s"Started  this.context.self.path : ${this.context.self.path.name} with ${this.context.self.path}")
--- src/main/scala/org/alakka/Trial.scala
+++ src/main/scala/org/alakka/Trial.scala
@@ -1,30 +1,50 @@
 package org.alakka
 
-import akka.actor.typed.Behavior
+import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+
+import org.alakka.Trial.Message
 
 
 
 object Trial {
-  sealed trait Command
-  case object Start extends Command
+  sealed trait Message
+  sealed trait Request extends Message
+  trait Response extends Message
+
+  case class StartRequest(credit:Int = 100 /*, replyTo: ActorRef[Trial.Response] */) extends Request
+  case class StartedResponse(rounds:Long,replyTo: ActorRef[Trial.Response] ) extends Response
+
   //final case class GetValue(replyTo: ActorRef[Value]) extends Command
   //final case class Value(n: Int)
 
-  def apply(): Behavior[Command] = {
+  def apply(): Behavior[Trial.Message] = {
     Behaviors.setup(context => new Trial(context))
   }
 }
-class Trial (context: ActorContext[Trial.Command]) extends AbstractBehavior[Trial.Command](context) {
+class Trial (context: ActorContext[Trial.Message]) extends AbstractBehavior[Trial.Message](context) {
 
-  import Trial._
+  //import Trial._
+  protected def run(initialCredit :Int) : Long = {
+      var round = 0L
+    var credit = initialCredit
 
-  private var n = 0
+    while(credit >= 0 ) {
+        round=round + 1
+        val isHead = scala.util.Random.nextBoolean()
+        if (isHead) credit+=1 else credit-=1
 
-  override def onMessage(msg: Command): Behavior[Command] = {
+      }
+      round
+  }
+  override def onMessage(msg: Message): Behavior[Message] = {
+
     msg match {
-      case Trial.Start =>
+      case Trial.StartRequest(credit) =>
         context.log.debug("Trial Started")
+        println(s"${this.context.self.path} : Starting with  $credit credits")
+        val roundsAchieved = this.run(credit)
+        println(s"${this.context.self.path} : Trial ended at coin toss #$roundsAchieved")
         this
 
     }
