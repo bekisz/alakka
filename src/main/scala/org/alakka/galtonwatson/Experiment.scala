c3c58473684d5a0f50aef63a505bba4c9039125b
Szabolcs Beki <szabi@apache.org>
2020-12-12 23:03:50 +0100

    Unsuccesful trail to simplfiy the dimensions

--- src/main/scala/org/alakka/galtonwatson/Experiment.scala
+++ src/main/scala/org/alakka/galtonwatson/Experiment.scala
@@ -3,7 +3,7 @@ package org.alakka.galtonwatson
 import org.alakka.utils.ProbabilityWithConfidence
 import org.alakka.utils.Time.time
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.sql.functions.{avg, format_number}
 import org.scalameter.utils.Statistics
 
@@ -23,13 +23,29 @@ object Experiment  {
 
       val trialsMultiplier:Int = if (args.size > 0)  { args(0).toInt } else 1000
 
+      val dimensionMap = Map[String,Seq[Any]] (
+          "lambda" -> (BigDecimal(1.0) to BigDecimal(1.6) by BigDecimal(0.1)) ,
+          "trialNo" -> (1 to trialsMultiplier))
+
+
+      /*
       val lambdas = for(lambda <- BigDecimal(1.0) to BigDecimal(1.6) by BigDecimal(0.1)) yield(lambda.toDouble)
       val trialsMultipliers = for( n <- 1 to trialsMultiplier) yield (n)
+      */
 
       import spark.implicits._
-      val lambdasDF =  spark.sparkContext.parallelize(lambdas).toDF.withColumnRenamed("value","lambda")
-      val trialsNosDF = spark.sparkContext.parallelize(trialsMultipliers).toDF.withColumnRenamed("value","trialNo")
-      val dimensionDS = lambdasDF.crossJoin(trialsNosDF).as[Dimension]
+      val dimRDD= dimensionMap.map{
+        case (name, range) => {
+          import spark.implicits._
+          spark.sparkContext.parallelize(range)
+        }
+      }.reduce((a,b) => a.cartesian(b))
+
+      //val lambdasDF =  spark.sparkContext.parallelize(lambdas).toDF.withColumnRenamed("value","lambda")
+      //val trialsNosDF = spark.sparkContext.parallelize(trialsMultipliers).toDF.withColumnRenamed("value","trialNo")
+
+
+      val dimensionDS:Dataset[Dimension] = dimRDD.map{ t => Dimension(t._1,t._2)}
 
       println(s"Galton-Watson Simulation started with ${dimensionDS.count()} trials")
 
