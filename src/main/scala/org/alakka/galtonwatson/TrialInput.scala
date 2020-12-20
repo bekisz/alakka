402130963be378a3bccccd213c9a5425ad765bcb
Szabolcs Beki <szabi@apache.org>
2020-12-20 11:22:04 +0100

    Update readme.md

--- src/main/scala/org/alakka/galtonwatson/TrialInput.scala
+++ src/main/scala/org/alakka/galtonwatson/TrialInput.scala
@@ -1,6 +1,15 @@
 package org.alakka.galtonwatson
 
-trait Dimension
+sealed trait Dimension
+
+trait HasTrialNo extends Dimension { val trialNo:Int }
+case class TrialNo(trialNo:Int) extends HasTrialNo
+object TrialNo {
+  def generateAllPossibleValues(maxTrialNo:Int = 1000): IndexedSeq[TrialNo]
+  = for(n <- 1 to maxTrialNo) yield TrialNo(n)
+}
+
+
 trait HasLambda extends Dimension { val lambda:Double }
 case class Lambda(lambda: Double) extends HasLambda
 object Lambda {
@@ -8,12 +17,6 @@ object Lambda {
     for (lambda <- BigDecimal(1.0) to BigDecimal(1.6) by BigDecimal(0.1)) yield Lambda(lambda.toDouble)
 }
 
-trait HasTrialNo extends Dimension { val trialNo:Int }
-case class TrialNo(trialNo:Int) extends HasTrialNo
-object TrialNo {
-  def generateAllPossibleValues(maxTrialNo:Int = 1000): IndexedSeq[TrialNo]
-    = for(n <- 1 to maxTrialNo) yield TrialNo(n)
-}
 
 case class TrialInput(override val lambda: Double,override val trialNo: Int) extends HasLambda with HasTrialNo {
   def constructTrial() :Trial =
