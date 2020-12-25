3566302ead52cdae1bae06c32ac704dcdcf02b74
Szabolcs Beki <szabi@apache.org>
2020-12-25 18:39:30 +0100

    Refactored TrialInputs and Dimensions

--- src/main/scala/org/alakka/galtonwatson/Experiment.scala
+++ src/main/scala/org/alakka/galtonwatson/Experiment.scala
@@ -8,30 +8,31 @@ import org.apache.spark.sql.{Dataset, SparkSession}
 
 
 
+class Experiment(val name:String, val multiplicity:Int = 1000, val lambdaRange:IndexedSeq[Double]= IndexedSeq(1.0)) {
+  val conf: SparkConf = new SparkConf().setAppName(name)
+  val spark: SparkSession = SparkSession.builder.config(conf).getOrCreate()
 
-object Experiment  {
-  val conf:SparkConf = new SparkConf().setAppName("Galton-Watson Simulation")
-  val spark:SparkSession = SparkSession.builder.config(conf).getOrCreate()
 
-  def generateAllTrialInputs(trialsMultiplier:Int =1000): Dataset[TrialInput] = {
+  def generateAllTrialInputs(): Dataset[TrialInput] = {
 
     import spark.implicits._
-    val lambdasDS = Lambda.generateAllPossibleValues().toDS()
-    val trialsNosDS = TrialNo.generateAllPossibleValues(trialsMultiplier).toDS()
-    lambdasDS.crossJoin(trialsNosDS).as[TrialInput]
+    val lambdasDS = this.lambdaRange.map(t => Lambda(t)).toDS()
+    val multiplicityDS = (for(i <- 1 to this.multiplicity) yield MultiplicityId(i) ).toDS()
+    lambdasDS.crossJoin(multiplicityDS).as[TrialInput]
 
   }
+
   // -- calculate and show expected extinction time by lambda
-  def showExpectedExtinctionTimesByLambda(trialOutputDS:Dataset[TrialOutput]):Unit = {
+  def showExpectedExtinctionTimesByLambda(trialOutputDS: Dataset[TrialOutput]): Unit = {
     import spark.implicits._
     trialOutputDS.where($"isSeedDominant" === false)
       .groupBy("lambda").agg(
-      format_number(avg($"time"),1).as("extinctionTime"))
+      format_number(avg($"time"), 1).as("extinctionTime"))
       .orderBy("lambda").show()
   }
 
   // -- calculate and show expected extinction time by lambda
-  def groupTrialOutputsByLambda(trialOutputDS:Dataset[TrialOutput], confidence:Double = 0.95):Dataset[TrialOutputByLambda] = {
+  def groupTrialOutputsByLambda(trialOutputDS: Dataset[TrialOutput], confidence: Double = 0.95): Dataset[TrialOutputByLambda] = {
 
     import spark.implicits._
     val survivalProb = trialOutputDS
@@ -41,18 +42,18 @@ object Experiment  {
       count($"isSeedDominant").as("noOfTrials"),
       sum($"time").as("sumOfTime"))
 
-    val survivalProbConf:Dataset[TrialOutputByLambda] = survivalProb.map {
+    val survivalProbConf: Dataset[TrialOutputByLambda] = survivalProb.map {
       row =>
         val lambda = row.getAs[Double](0)
-        val survivalProbability =row.getAs[Double](1)
-        val stdDevProbability =row.getAs[Double](2)
-        val noOfTrials =row.getAs[Long](3)
+        val survivalProbability = row.getAs[Double](1)
+        val stdDevProbability = row.getAs[Double](2)
+        val noOfTrials = row.getAs[Long](3)
 
         val (confidenceIntervalLow, confidenceIntervalHigh)
-        = Statistics.confidenceInterval (noOfTrials, survivalProbability, stdDevProbability, confidence)
+        = Statistics.confidenceInterval(noOfTrials, survivalProbability, stdDevProbability, confidence)
 
-        TrialOutputByLambda(lambda, ProbabilityWithConfidence(survivalProbability, confidence,confidenceIntervalLow, confidenceIntervalHigh),
-          noOfTrials, row.getAs[Long](4) )
+        TrialOutputByLambda(lambda, ProbabilityWithConfidence(survivalProbability, confidence, confidenceIntervalLow, confidenceIntervalHigh),
+          noOfTrials, row.getAs[Long](4))
     }.sort($"lambda")
 
     survivalProbConf
@@ -61,39 +62,43 @@ object Experiment  {
   // Something like this to stdout :
   // 8888 ticks (unit of time) processed in 700 trials, averaging 12.7 ticks/trial
 
-  def showPerformanceMetrics(trialOutputDS:Dataset[TrialOutput]):Unit = {
+  def showPerformanceMetrics(trialOutputDS: Dataset[TrialOutput]): Unit = {
     val sumOfTime = trialOutputDS.agg(sum("time")).first().getAs[Long](0)
     val totalTrials = trialOutputDS.agg(count("time")).first().getAs[Long](0)
 
-    println(s"\n$sumOfTime ticks (unit of time) processed in $totalTrials trials, averaging "+ f"${sumOfTime.doubleValue()/totalTrials}%1.1f ticks/trial\n")
+    println(s"\n$sumOfTime ticks (unit of time) processed in $totalTrials trials, averaging " + f"${sumOfTime.doubleValue() / totalTrials}%1.1f ticks/trial\n")
   }
+}
+object Experiment {
 
   def main(args : Array[String]): Unit = {
 
     time {
+      val multiplicity = if (args.length > 0)  { args(0).toInt } else 1000
+      val lambdaRange = IndexedSeq(1.0, 1.1,1.2,1.3,1.4,1.5,1.6)
 
-      val trialsMultiplier:Int = if (args.length > 0)  { args(0).toInt } else 1000
+      val experiment= new Experiment("Galton-Watson Experiment", multiplicity, lambdaRange);
 
-      val trialInputDS = this.generateAllTrialInputs(trialsMultiplier)
+      val trialInputDS = experiment.generateAllTrialInputs()
 
-      println(s"${spark.sparkContext.appName} started with ${trialInputDS.count()} trials")
-      println("Spark version : " + spark.sparkContext.version)
-      import spark.implicits._
+      println(s"${experiment.spark.sparkContext.appName} started with ${trialInputDS.count()} trials")
+      println("Spark version : " + experiment.spark.sparkContext.version)
+      import experiment.spark.implicits._
       val trialOutputDS = trialInputDS
         .map(trialInput =>
-          TrialOutput(trialInput.constructTrial().run()) )
+          TrialOutput( new Trial(maxPopulation = 1000, seedNode = new Node(trialInput.lambda)).run()) )
         .cache()
 
 
-      this.showExpectedExtinctionTimesByLambda(trialOutputDS)
+      experiment.showExpectedExtinctionTimesByLambda(trialOutputDS)
 
       val confidence = 0.99
       println(s"\nSurvival Probabilities within ${confidence*100}% confidence interval by lambdas" )
-      this.groupTrialOutputsByLambda(trialOutputDS, confidence)
+      experiment.groupTrialOutputsByLambda(trialOutputDS, confidence)
         .collect().foreach( aggregatedOutput => println(aggregatedOutput.toString()))
 
-      this.showPerformanceMetrics(trialOutputDS)
-      spark.stop()
+      experiment.showPerformanceMetrics(trialOutputDS)
+      experiment.spark.stop()
 
 
     }
